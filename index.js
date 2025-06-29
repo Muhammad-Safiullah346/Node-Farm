bc655d50545f47012278e4ba6b3f9dad63814557
Muhammad-Safiullah346 <akbarsafiullah32@gmail.com>
2025-06-29 08:23:03 -0700

    Solved Deployment issues

--- index.js
+++ index.js
@@ -1,127 +1,81 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-// const slugify = require('slugify');
-const replaceTemplate = require('./modules/replaceTemplate');
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-/////////////////////////////////
-// FILES
-
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written!');
-
-// Non-blocking, asynchronous way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log('ERROR! 💥');
-
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//         console.log('Your file has been written 😁');
-//       })
-//     });
-//   });
-// });
-// console.log('Will read file!');
-
-// http.createServer((req, res) => {
-//   res.end('Hello from the server! 🥳');
-// }).listen(8000, '127.0.0.1', () => {
-//   console.log('Waiting for requests on port 8000');
-// });
-
-/////////////////////////////////
-// SERVER
-
-// Reading HTML files
+// Reading HTML files with absolute paths
 const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
+  path.join(__dirname, "templates/template-overview.html"),
+  "utf-8"
 );
 const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
+  path.join(__dirname, "templates/template-card.html"),
+  "utf-8"
 );
 const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
+  path.join(__dirname, "templates/template-product.html"),
+  "utf-8"
 );
 
-// Reading CSS file
+// Reading CSS files with absolute paths
 const tempProductStyle = fs.readFileSync(
-  `${__dirname}/templates/public/template-product.css`,
-  'utf-8'
+  path.join(__dirname, "templates/public/template-product.css"),
+  "utf-8"
 );
 const tempOverviewStyle = fs.readFileSync(
-  `${__dirname}/templates/public/template-overview.css`,
-  'utf-8'
+  path.join(__dirname, "templates/public/template-overview.css"),
+  "utf-8"
 );
 
-// Reading JSON file
-const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+// Reading JSON file with absolute path
+const data = fs.readFileSync(
+  path.join(__dirname, "dev-data/data.json"),
+  "utf-8"
+);
 const dataObj = JSON.parse(data);
 
-// const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
-// console.log(slugs);
+// Export the handler function for Vercel
+module.exports = (req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-const server = http
-  .createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
+  // Serve CSS files
+  if (pathname === "/public/template-overview.css") {
+    res.writeHead(200, { "Content-type": "text/css" });
+    res.end(tempOverviewStyle);
+  } else if (pathname === "/public/template-product.css") {
+    res.writeHead(200, { "Content-type": "text/css" });
+    res.end(tempProductStyle);
 
-    // Serve CSS file
-    if (pathname === '/public/template-overview.css') {
-      res.writeHead(200, { 'Content-type': 'text/css' });
-      res.end(tempOverviewStyle);
-    } else if (pathname === '/public/template-product.css') {
-      res.writeHead(200, { 'Content-type': 'text/css' });
-      res.end(tempProductStyle);
-      // Overview page
-    } else if (pathname === '/' || pathname === '/overview') {
-      res.writeHead(200, {
-        'Content-type': 'text/html',
-      });
+    // Overview page
+  } else if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
 
-      const cardsHtml = dataObj
-        .map((el) => replaceTemplate(tempCard, el))
-        .join('');
-      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-      res.end(output);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
-      // Product page
-    } else if (pathname === '/product') {
-      res.writeHead(200, {
-        'Content-type': 'text/html',
-      });
-      const product = dataObj[query.id];
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    if (product) {
       const output = replaceTemplate(tempProduct, product);
       res.end(output);
-
-      // API
-    } else if (pathname === '/api') {
-      res.writeHead(200, {
-        'Content-type': 'application/json',
-      });
-      res.end(data);
-      // Not found
     } else {
-      res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world',
-      });
-      res.end('<h1>Page not found!</h1>');
+      res.writeHead(404, { "Content-type": "text/html" });
+      res.end("<h1>Product not found!</h1>");
     }
-  })
-  .listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
-  });
-
-// server.listen(8000, '127.0.0.1', () => {
-//   console.log('Listening to requests on port 8000');
-// });
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+};
